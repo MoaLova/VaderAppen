540ff1b1cd84a223f70a38af140b222d932c153d
luma <lucas.martinahlm@gmail.com>
2024-02-29 15:09:41 +0100

    placeholder vy för första sidan

--- VaderAppen/ContentView.swift
+++ VaderAppen/ContentView.swift
@@ -8,79 +8,57 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+struct ContentView: View{
+    var body: some View{
+        VStack{
+            HStack{
+                Text("07.00")
+                
+                Button(action: {
+                    print("nein")
+                    
+                }){Text("Heart")}
+                
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                
+                
+                
+                
+                Text("Location")
+                
+                
+                Rectangle()
+                
+                    .stroke(Color.black, lineWidth: 3)
+                    .frame(width: 340, height: 550)
+                    .foregroundColor(.white)
+                    .position(x: 200, y: 300)
+                
+                
+                
+                
+                
+                HStack{
+                    
+                    Rectangle()
+                        .stroke(Color.black, lineWidth:3)
+                        .frame(width: 100, height:100)
+                        .position(x: 80, y: 300)
+                    
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+                
+                
             }
-            Text("Select an item")
+            
+            
+            
+            
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider{
+        static var previews: some View{
+            ContentView()
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-}
