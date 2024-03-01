a7685478f4f3894c2e79e2d3bd6c1d8c3119e03a
”Moa <svensson.moaa@gmail.com>
2024-03-01 15:49:02 +0100

    Saved locations med Lukas.

---
+++ VaderAppen/SavedLocationItemView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedLocationItemView.swift
+//  VaderAppen
+//
+//  Created by Moa.Svensson on 2024-03-01.
+//
+
+import SwiftUI
+
+struct SavedLocationItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedLocationItemView()
+}
