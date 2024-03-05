62aec357f9bd097792fff066a46597cc114b3fef
Leo <leonardo.ahlm@hotmail.com>
2024-03-05 14:02:02 +0100

    Startat CoreData för quiz.

---
+++ VaderAppen/QuizCoreData.swift
@@ -0,0 +1,18 @@
+//
+//  QuizCoreData.swift
+//  VaderAppen
+//
+//  Created by Leonardo.Ahlm on 2024-03-05.
+//
+
+import SwiftUI
+
+struct QuizCoreData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizCoreData()
+}
