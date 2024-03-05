e436b590800d290ad416e2274a62c13d4bb8c4dd
”Moa <svensson.moaa@gmail.com>
2024-03-05 21:58:00 +0100

    Test som ej funkar än

---
+++ VaderAppen/CloudView.swift
@@ -0,0 +1,28 @@
+//
+//  CloudView.swift
+//  VaderAppen
+//
+//  Created by Moa.Svensson on 2024-03-05.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct CloudView: View {
+    var locationSaved: SavedLocation
+    var direction: CGFloat
+
+    var body: some View {
+        Image(systemName: "cloud")
+            .resizable()
+            .foregroundColor(.blue)
+            .frame(width: 40, height: 40)
+            .overlay(
+                Text(locationSaved.location)
+                    .foregroundColor(.black)
+                    .font(.headline)
+            )
+            .modifier(FlyingCloudsModifier(position: CGPoint(x: 200, y: -10), direction: direction))
+    }
+}
---
+++ VaderAppen/FlyingCloudsModifier.swift
@@ -0,0 +1,29 @@
+//
+//  FlyingCloudsModifier.swift
+//  VaderAppen
+//
+//  Created by Moa.Svensson on 2024-03-05.
+//
+
+import Foundation
+import SwiftUI
+
+struct FlyingCloudsModifier: AnimatableModifier {
+    var position: CGPoint
+    var direction: CGFloat
+
+    var animatableData: CGPoint {
+        get { position }
+        set { position = newValue }
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .position(position)
+            .onAppear {
+                withAnimation(Animation.easeInOut(duration: 2.0).repeatForever()) {
+                    self.position.x += 200 * self.direction
+                }
+            }
+    }
+}
