2d3e61aba64f0ae36ce040b498d876202ff8713a
”Moa <svensson.moaa@gmail.com>
2024-03-07 10:49:38 +0100

    FlyingCloudsModifier

---
+++ VaderAppen/FlyingCloudsModifier.swift
@@ -0,0 +1,31 @@
+//
+//  CloudModifier.swift
+//  VaderAppen
+//
+//  Created by Moa.Svensson on 2024-03-07.
+//
+
+import Foundation
+import SwiftUI
+
+struct FlyingCloudsModifier: AnimatableModifier {
+    var position: CGPoint
+    var direction: CGFloat
+
+    var animatableData: CGPoint {
+        get { position }
+        set { position = newValue }
+    }
+
+    func body(content: Content) -> some View {
+        var position: CGPoint = self.position
+        let direction: CGFloat = self.direction
+        content
+            .position(position)
+            .onAppear {
+                withAnimation(Animation.easeInOut(duration: 2.0).repeatForever()) {
+                    position.x += 200 * direction
+                }
+            }
+    }
+}
