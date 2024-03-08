4f226c2da0841aabbafa53e535777358dc1ec250
”Moa <svensson.moaa@gmail.com>
2024-03-08 23:28:04 +0100

    Frågorna hårdkodade

--- VaderAppen/QuestionViewModel.swift
+++ VaderAppen/QuestionViewModel.swift
@@ -9,14 +9,14 @@ import Foundation
 
 
 class QuestionsViewModel : ObservableObject {
-    var questions = [Question(question: "När är julafton?", correctAnswer: "24 dec",
-                              incorrectAnswers: ["1 jan", "6 maj", "9 dec"]),
-                     Question(question: "Vilket är bäst?", correctAnswer: "Zoom",
-                                               incorrectAnswers: ["discord", "teams", "slack"]),
-                     Question(question: "Vilket är svårast?", correctAnswer: "Java",
-                                               incorrectAnswers: ["swift", "kotlin", "javascript"]),
-                     Question(question: "Hur högt är eifeltornet", correctAnswer: "324 m",
-                                               incorrectAnswers: ["34 m", "1045 m", "456 m"])
+    var questions = [Question(question: "Vad är den genomsnittliga temperaturen på jorden?", correctAnswer: "15 grader Celsius",
+                              incorrectAnswers: ["0 grader Celsius", "10 grader Celsius", "20 grader Celsius"]),
+                     Question(question: "Vilken atmosfärisk komponent är mest ansvarig för växthuseffekten?", correctAnswer: "Vattenånga",
+                                               incorrectAnswers: ["Kväve", "Syre", "Koldioxid"]),
+                     Question(question: "Vilken är den högsta dokumenterade temperaturen på jorden?", correctAnswer: "50 grader Celsius",
+                                               incorrectAnswers: ["75 grader Celsius", "60 grader Celsius", "85 grader Celsius"]),
+                     Question(question: "Vad kallas en kraftig virvelvind över vatten som kan vara förknippad med kraftiga stormar?", correctAnswer: "Orkan",
+                        incorrectAnswers: ["Tromb", "Tornado", "Tyfon"])
                     ]
     
     @Published var currentQuestion : Question
--- VaderAppen/QuizView.swift
+++ VaderAppen/QuizView.swift
@@ -7,86 +7,99 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct QuizView: View {
     @State var text: String = ""
     @StateObject var questionsVM = QuestionsViewModel()
-
+    
     var body: some View {
+        
+        Text("Weather Quiz")
+            .font(.title)
+            .padding(20)
+        Spacer()
+        
         VStack {
-            HStack {
-                Text("07.00")
-                    .position(x: 50, y: 10)
-                
-                Button(action: {
-                    print("nein")
-                }) {
-                    Image(systemName: "cloud.sun")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .frame(width: 70, height: 50)
-                        .position(x: 130, y: 10)
-                }
+            Image(systemName: "rainbow")
+                .resizable()
+                .frame(width: 300, height: 150)
+                .symbolRenderingMode(.multicolor)
+            
+            Text(questionsVM.currentQuestion.question)
+                           .padding(10)
+                           .fixedSize(horizontal: false, vertical: true)
+            Spacer()
+            
+            Button(action: {
+                handleAnswer(answer: questionsVM.currentQuestion.correctAnswer)
+            }) {
+                Image(systemName:"sun.max.fill")
+                    .resizable()
+                    .foregroundColor(.yellow)
+                    .frame(width: 80, height: 80)
+                    .padding()
+                Text(questionsVM.currentQuestion.correctAnswer)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                   
             }
             
-            Text("Weather Quiz")
-                .position(x: 190, y: -180)
-                .font(.title)
-            
-            VStack {
-                Image(systemName: "rainbow")
-                                  .resizable()
-                                  .frame(width: 300, height: 150)
-                                  .position(x: 200, y: -330)
-                                  .symbolRenderingMode(.multicolor)
-                              Text(questionsVM.currentQuestion.question).position(x: 200, y: -310)
-                              
-              
-               
-                Button(action: {
-                    handleAnswer(answer: questionsVM.currentQuestion.correctAnswer)
-                }) {
-                    Text(questionsVM.currentQuestion.correctAnswer)
-                }
-                Button(action: {
-                    handleAnswer(answer: questionsVM.currentQuestion.incorrectAnswers[0])
-                }) {
-                    Text(questionsVM.currentQuestion.incorrectAnswers[0])
-                }
-                Button(action: {
-                    handleAnswer(answer: questionsVM.currentQuestion.incorrectAnswers[1])
-                }) {
-                    Text(questionsVM.currentQuestion.incorrectAnswers[1])
-                }
-                Button(action: {
-                    handleAnswer(answer: questionsVM.currentQuestion.incorrectAnswers[2])
-                }) {
-                    Text(questionsVM.currentQuestion.incorrectAnswers[2])
-                }
-              
-                
+            Button(action: {
+                handleAnswer(answer: questionsVM.currentQuestion.incorrectAnswers[0])
+            }) {
+                Image(systemName:"drop.fill")
+                .resizable()
+                .foregroundColor(.blue)
+                .frame(width: 60, height: 70)
+                .padding()
+                Text(questionsVM.currentQuestion.incorrectAnswers[0])
+                    .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .onAppear {
-                nextQuestion()
-            }}}
-
+            
+            Button(action: {
+                handleAnswer(answer: questionsVM.currentQuestion.incorrectAnswers[1])
+            }) {
+                Image(systemName:"wind")
+                 .resizable()
+                   .foregroundColor(.black)
+                    .frame(width: 80, height: 80)
+                    .padding()
+                Text(questionsVM.currentQuestion.incorrectAnswers[1])
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            
+            Button(action: {
+                handleAnswer(answer: questionsVM.currentQuestion.incorrectAnswers[2])
+            }) {
+                Image(systemName:"icloud.fill")
+                  .resizable()
+                  .foregroundColor(.gray)
+                  .frame(width: 100, height: 80)
+                  .padding()
+                Text(questionsVM.currentQuestion.incorrectAnswers[2])
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            Spacer()
+            
+            
+        }.onAppear {
+            nextQuestion()
+        }
+        
+    }
+    
     func handleAnswer(answer: String) {
         // Implement your answer handling logic here
         print("Selected answer: \(answer)")
         // You might want to check if the answer is correct and proceed accordingly
+        nextQuestion()
     }
 
     func nextQuestion() {
         // Implement logic to load the next question
         // This might involve updating your QuestionsViewModel
+        questionsVM.nextQuestion()
     }
 }
-
-#if DEBUG
-struct QuizView_Previews: PreviewProvider {
-    static var previews: some View {
+    #Preview {
         QuizView()
     }
-}
-#endif
+
